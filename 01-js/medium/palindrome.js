ecba4b1a33a9b0a027b894a5ac4bb06425504b72
Abhishek Choudhury <abhishek.choudhury2697@gmail.com>
2023-06-17 15:48:57 +0530

    solve palindrome.js in O(n)

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,6 +7,23 @@
 */
 
 function isPalindrome(str) {
+  let left = 0;
+  let right = str.length - 1;
+  const skipCharacters = new Set("?.!,; ");
+  while (left <= right) {
+    if (skipCharacters.has(str[left])) {
+      left += 1
+      continue
+    }
+    if (skipCharacters.has(str[right])) {
+      right -= 1
+      continue
+    }
+    if (str[left].toLowerCase() !== str[right].toLowerCase())
+      return false;
+    left += 1;
+    right -= 1;
+  }
   return true;
 }
 
